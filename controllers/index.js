1b4cdd1d5bd71ace460e8b395f704939de129e15
Phong Hoang <p.hoang6900@gmail.com>
2023-02-09 18:46:06 -0500

    Added read operator and added route

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,18 @@ const createListing = async (req, res) => {
     return res.status(500).json({ error: error.message })
   }
 }
+const getListingById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const listing = await Listing.findById(id)
+    if (listing) {
+      return res.status(200).json({ listing })
+    }
+    return res.status(404).send('Requested listing does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 const updateListing = async (req, res) => {
   try {
     const listing = await Listing.findByIdAndUpdate(req.params.id, req.body, {
@@ -22,7 +34,9 @@ const updateListing = async (req, res) => {
     return res.status(500).send(error.message)
   }
 }
+
 module.exports = {
   createListing,
-  updateListing
+  updateListing,
+  getListingById
 }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = Router()
 const controllers = require('../controllers')
 
 router.get('/', (req, res) => res.send('Home'))
+router.get('/listing/:id', controllers.getListingById)
 router.post('/listing', controllers.createListing)
 router.put('/listing/:id', controllers.updateListing)
 module.exports = router
