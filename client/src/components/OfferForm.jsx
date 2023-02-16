5b99666394f73613b259322f87c81b787f7178e6
Phong Hoang <p.hoang6900@gmail.com>
2023-02-16 10:33:54 -0500

    Rendered offers on details page after offer gets added

--- client/src/components/OfferForm.jsx
+++ client/src/components/OfferForm.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const OfferForm = (props) => {
@@ -18,7 +18,7 @@ const OfferForm = (props) => {
     } catch (err) {
       console.log(err)
     }
-    console.log(offers)
+    console.log(offers.offers)
   }
 
   const handleChange = (event) => {
@@ -31,40 +31,52 @@ const OfferForm = (props) => {
     setFormState(initialState)
     getOffers()
   }
-
+  useEffect(() => {
+    getOffers()
+  }, [])
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="buyer">Buyer:</label>
-      <input
-        id="buyer"
-        type="text"
-        onChange={handleChange}
-        value={formState.buyer}
-      ></input>
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        type="text"
-        onChange={handleChange}
-        value={formState.email}
-      ></input>
-      <label htmlFor="offer">Offer:</label>
-      <input
-        id="offer"
-        type="number"
-        onChange={handleChange}
-        value={formState.offer}
-      ></input>
-      <label htmlFor="comments">Additional Comments:</label>
-      <input
-        id="comments"
-        type="text"
-        onChange={handleChange}
-        value={formState.comments}
-      ></input>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="buyer">Buyer:</label>
+        <input
+          id="buyer"
+          type="text"
+          onChange={handleChange}
+          value={formState.buyer}
+        ></input>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="text"
+          onChange={handleChange}
+          value={formState.email}
+        ></input>
+        <label htmlFor="offer">Offer:</label>
+        <input
+          id="offer"
+          type="number"
+          onChange={handleChange}
+          value={formState.offer}
+        ></input>
+        <label htmlFor="comments">Additional Comments:</label>
+        <input
+          id="comments"
+          type="text"
+          onChange={handleChange}
+          value={formState.comments}
+        ></input>
 
-      <button type="submit">Add</button>
-    </form>
+        <button type="submit">Add</button>
+      </form>
+      {offers.offers.map((offer) => (
+        <div key={offer._id}>
+          <h3>Buyer: {offer.buyer}</h3>
+          <h3>Email: {offer.email}</h3>
+          <h3>Offer: {offer.offer}</h3>
+          <h3>Additional Comments: {offer.comments}</h3>
+        </div>
+      ))}
+    </div>
   )
 }
 
